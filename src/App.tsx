79f0cac3b9c655ed7b95152998282da80387011e
mneifar <mahdi.neifar123@gmail.com>
2024-02-13 12:25:04 +0100

    install toast library

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 import { registerRootComponent } from "expo";
 import { NavigationContainer } from "@react-navigation/native";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import {
+  ConfigType,
+  ToastPosition,
+  ToastProvider,
+} from "react-native-toast-animated";
 import migrations from "../db/outputs/migrations";
 import { DrawerNavigator } from "./navigation/DrawerNavigator";
 import { DarkTheme } from "./shared/DarkTheme";
@@ -9,15 +14,18 @@ import { db } from "./sqlite/DrizzleDB";
 
 import "./i18n/i18n.config";
 
-const App = () => {
-  const { success, error } = useMigrations(db, migrations);
+const config: ConfigType = {
+  position: ToastPosition.TOP,
+};
 
-  console.log(success);
-  console.log(error);
+const App = () => {
+  useMigrations(db, migrations);
 
   return (
     <NavigationContainer theme={DarkTheme}>
-      <DrawerNavigator />
+      <ToastProvider config={config}>
+        <DrawerNavigator />
+      </ToastProvider>
     </NavigationContainer>
   );
 };
